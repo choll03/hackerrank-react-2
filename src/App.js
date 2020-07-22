e3c2beb56e3f9598a16666ada727ae03d074ab35
mail <ismail15000000@gmail.com>
2020-07-22 23:18:22 +0700

    update

--- src/App.js
+++ src/App.js
@@ -8,14 +8,11 @@ const title = "Sorting Articles";
 
 function App({articles}) {
 
+
     return (
         <div className="App">
             <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
-            </div>
+            
             <Articles articles={articles}/>
         </div>
     );
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,8 +1,38 @@
 import React from 'react';
 
-function Articles() {
+class Articles extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            articles: props.articles
+        }
+    }
+
+    sortUpvoted(datas){
+        
+        this.setState({
+            articles: datas.sort((a, b) => b.upvotes - a.upvotes)
+        })
+    }
+
+    sortUpdate(datas) {
+        this.setState({
+            articles: datas.sort((a,b) => new Date(b.date) - new Date(a.date))
+        })
+    }
+
+
+    render() {
+        
+    
     return (
+        <>
+        <div className="layout-row align-items-center justify-content-center my-20 navigation">
+            <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
+            <button data-testid="most-upvoted-link" className="small" onClick={() => this.sortUpvoted(this.state.articles)}>Most Upvoted</button>
+            <button data-testid="most-recent-link" className="small" onClick={() => this.sortUpdate(this.state.articles)}>Most Recent</button>
+        </div>
         <div className="card w-50 mx-auto">
             <table>
                 <thead>
@@ -13,16 +43,21 @@ function Articles() {
                 </tr>
                 </thead>
                 <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
+                    {
+                        this.state.articles.map((article, i) => (
+                            <tr data-testid="article" key={i}>
+                                <td data-testid="article-title">{article.title}</td>
+                                <td data-testid="article-upvotes">{article.upvotes}</td>
+                                <td data-testid="article-date">{article.date}</td>
+                            </tr>
+                        ))
+                    }
                 </tbody>
             </table>
         </div>
+        </>
     );
 
 }
-
+}
 export default Articles;
